18bbaf4e27e50b1ee8ae2a36a6082fbf2f97926c
Guizao <84868817+guioverflow@users.noreply.github.com>
2023-12-03 18:06:34 -0300

    Update avl.h

--- trees/avl/avl.h
+++ trees/avl/avl.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 typedef struct NoAVL {
     struct NoAVL* pai;
@@ -36,35 +35,3 @@ NoAVL* rse(ArvoreAVL* arvore, NoAVL* no, ContadorAVL* contadorAVL);
 NoAVL* rsd(ArvoreAVL* arvore, NoAVL* no, ContadorAVL* contadorAVL);
 NoAVL* rde(ArvoreAVL* arvore, NoAVL* no, ContadorAVL* contadorAVL);
 NoAVL* rdd(ArvoreAVL* arvore, NoAVL* no, ContadorAVL* contadorAVL);
-=======
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct NoAVL {
-    struct NoAVL* pai;
-    struct NoAVL* esquerda;
-    struct NoAVL* direita;
-    int valor;
-} NoAVL;
-
-typedef struct arvore {
-    struct NoAVL* raiz;
-} ArvoreAVL;
-
-ArvoreAVL* criarAVL();
-int vazia(ArvoreAVL* arvore);
-NoAVL* criarNoAVL(int valor);
-NoAVL* adicionarNoAVL(NoAVL* NoAVL, int valor);
-NoAVL* adicionar(ArvoreAVL* arvore, int valor);
-void balanceamento(ArvoreAVL* a, NoAVL* NoAVL);
-NoAVL* localizar(NoAVL* NoAVL, int valor);
-void percorrer(NoAVL* NoAVL, void (*callback)(int));
-void visitar(int valor);
-int altura(NoAVL* NoAVL);
-int fb(NoAVL* NoAVL);
-
-NoAVL* rse(ArvoreAVL* arvore, NoAVL* NoAVL);
-NoAVL* rsd(ArvoreAVL* arvore, NoAVL* NoAVL);
-NoAVL* rde(ArvoreAVL* arvore, NoAVL* NoAVL);
-NoAVL* rdd(ArvoreAVL* arvore, NoAVL* NoAVL);
->>>>>>> e6a5317d1fd026924e6d3c70542553efe7572334
